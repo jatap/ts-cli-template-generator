8b2fa3c65792b47d416a2d0457d432aa9d4d8fbb
Julio Antúnez Tarín <julio@giffgaff.co.uk>
2018-11-11 22:13:57 +0000

    feat(packages): deprecate mocha, use jest

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,22 @@
+import { spawnSync } from "child_process"
+
+describe("main", () => {
+  let execution
+
+  beforeAll(async () => {
+    execution = await spawnSync(
+      "node_modules/.bin/babel-node",
+      ["src/index.js"],
+      {
+        stdio: "pipe",
+        encoding: "utf-8"
+      }
+    )
+  })
+
+  test("prints stdout", () => {
+    const result = "Hello Julio"
+
+    expect(execution.stdout.toString().trim()).toBe(result)
+  })
+})
