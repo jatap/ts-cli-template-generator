af477c122206dfcb296471931441e3d1d570e77b
Julio Antúnez Tarín <julio@giffgaff.co.uk>
2020-03-26 17:07:32 +0000

    refactor(packages): update

--- src/cli.ts
+++ src/cli.ts
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 
-const program = require("commander");
+const program = require('commander');
 
-program
-  .version("0.1.0", "-v, --version")
-  .name("XXX")
-  .description("XXX");
+program.version('0.1.0', '-v, --version').name('XXX').description('XXX');
 
-program.on("--help", () => {
-  console.log("");
-  console.log("Examples:");
-  console.log("  $ XXX --help");
-  console.log("  $ XXX -h");
+program.on('--help', () => {
+  console.log('');
+  console.log('Examples:');
+  console.log('  $ XXX --help');
+  console.log('  $ XXX -h');
 });
 
 program.parse(process.argv);
